a287009b6dab4e41384e981d95ab17caaadc39fd
bayanfuad <bayanjawawdeh9@gmail.com>
2022-08-09 20:31:49 +0300

    add auth

--- src/About.js
+++ src/About.js
@@ -1,11 +1,33 @@
 import { Component } from "react";
-
-class Profile extends Component {
+import Table from 'react-bootstrap/Table';
+class Profileu extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <>
+       <Table striped bordered hover>
+      <thead>
+        <tr>
+          Developer Information
+        </tr>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Contact me at </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Bayan</td>
+          <td>Al-Jawawdeh</td>
+          <td><a href="https://github.com/bayanfuad">Github</a></td>
+        </tr>
+      </tbody>
+    </Table>
+      </>
+    )
   }
 };
 
-export default Profile;
+export default Profileu;
